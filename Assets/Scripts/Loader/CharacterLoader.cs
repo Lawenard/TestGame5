3de84bb84676242242f1ea28ec44e5aa9706f9a7
Lawenard <max.v.yur@gmail.com>
2025-07-08 18:03:45 +0300

    character loader

---
+++ Assets/Scripts/Loader/CharacterLoader.cs
@@ -0,0 +1,14 @@
+﻿namespace Loader
+{
+    using UnityEngine;
+
+    public static class CharacterLoader
+    {
+        private const string CharactersPath = "Characters";
+        
+        public static Sprite Load(string character, string variant)
+        {
+            return Resources.Load<Sprite>(string.Join('/', CharactersPath, character, variant));
+        }
+    }
+}
