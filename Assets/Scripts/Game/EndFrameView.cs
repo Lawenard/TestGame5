565d638e41a836dac31c4de4b6686f3767eb31b5
Lawenard <max.v.yur@gmail.com>
2025-07-08 22:09:07 +0300

    update settings

--- Assets/Scripts/Game/EndFrameView.cs
+++ Assets/Scripts/Game/EndFrameView.cs
@@ -11,13 +11,17 @@
     {
         [SerializeField] private TextMeshProUGUI text;
         [SerializeField] private Image image;
-
+        [SerializeField] private Button nextButton;
+        
         public override void View(EndFrame frame, Action<string> callback)
         {
             base.View(frame, callback);
             
             text.text = frame.Text;
             image.sprite = ImageLoader.LoadEndingImage(frame.ImageId);
+            
+            nextButton.onClick.RemoveAllListeners();
+            nextButton.onClick.AddListener(() => OnExit?.Invoke(string.Empty));
         }
     }
 }
