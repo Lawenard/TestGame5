e47238a0fc3d7c0cafbfaf3a3636abd12d193cbf
Lawenard <max.v.yur@gmail.com>
2025-07-08 18:05:03 +0300

    speech frame view initial logic

--- Assets/Scripts/Game/SpeechFrameView.cs
+++ Assets/Scripts/Game/SpeechFrameView.cs
@@ -2,18 +2,31 @@
 {
     using System;
     using Frames;
+    using Loader;
     using TMPro;
     using UnityEngine;
+    using UnityEngine.UI;
 
     public class SpeechFrameView : FrameView<SpeechFrame>
     {
-        [SerializeField] private TextMeshProUGUI text;
-
+        [SerializeField] private TextMeshProUGUI speechText;
+        [SerializeField] private TextMeshProUGUI nameText;
+        [SerializeField] private Image characterImage;
+        [SerializeField] private Transform leftCharacterRoot;
+        [SerializeField] private Transform rightCharacterRoot;
+        
         public override void View(SpeechFrame frame, Action<string> callback)
         {
             base.View(frame, callback);
             
-            text.text = frame.Text;
+            speechText.text = frame.Text;
+            
+            nameText.text = frame.Name;
+            nameText.alignment = frame.IsRightSide ? TextAlignmentOptions.Right : TextAlignmentOptions.Left;
+
+            characterImage.sprite = CharacterLoader.Load(frame.Name, frame.SpriteId);
+            characterImage.rectTransform.parent = frame.IsRightSide ? rightCharacterRoot : leftCharacterRoot;
+            characterImage.rectTransform.anchoredPosition = Vector2.zero;
         }
     }
 }
